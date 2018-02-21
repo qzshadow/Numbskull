caa6c10e875d8d376ad03310d8c5cc16c03654da
qzshadow <qz1431880590@gmail.com>
2018-02-20 21:32:54 -0500

    Update the Input to create a BDC graph, with variable 0, 1 on machine 0, variable 2, 3 on machine 1, variable 4, 5 on machine 2

--- README.md
+++ README.md
@@ -17,3 +17,39 @@ cd make-build-debug
 make run
 ```
 
+## Input Format
+#### Factors
+|factor_id|assign|var_id|factor_type|weight|
+
+factor_id: long int, starts from 0
+
+assign: string (e.g. "D1" means the factor belongs to type 'D', assign to machine 1)
+
+var_id: long int, variable this factor connected to, starts from 0
+
+weight: double
+
+#### Variables
+|var_id|assign|init_val|factor_id|
+
+var_id: long int, starts from 0
+
+assign: string(e.g. "B0" means the factor belongs to type 'B', assigned to machine 0)
+
+init_val: categorical initial value for this variable
+
+factor_id: long int, factos this variable connected to, starts from 0
+
+
+#### GraphInfo
+|assign|var_start_idx|num_vars|fac_start_idx|num_factors|
+
+assign: string
+
+var_start_idx: long int, the start index of variable in this machine
+
+num_vars: long int, number of variables in this machine
+
+fac_start_idx: long int, the start index of factor in this machine
+
+num_facs: long int, number of factors in this machine
--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@ enum class MessageType {
     Info, Var, Fac
 };
 
+int num_samples = 1;
+
 int main() {
     mpi::environment env;
     mpi::communicator world;
@@ -30,10 +32,10 @@ int main() {
                 std::vector<Variable> vec_vars = std::vector<Variable>(graph.variables_map[assign]->begin(),
                                                                        graph.variables_map[assign]->begin() +
                                                                        info.num_variables);
-                std::cout << "try send variables to " << machine_index << std::endl;
-                for (auto var : vec_vars) std::cout << var << std::endl;
+                //std::cout << "try send variables to " << machine_index << std::endl;
+                //for (auto& var : vec_vars) std::cout << var << std::endl;
                 world.send(machine_index, static_cast<int>(MessageType::Var), vec_vars);
-                std::cout << "send variables to " << machine_index << " finished" << std::endl;
+                //std::cout << "send variables to " << machine_index << " finished" << std::endl;
             }
 
 
@@ -42,29 +44,46 @@ int main() {
                 std::vector<Factor> vec_facs = std::vector<Factor>(graph.factors_map[assign]->begin(),
                                                                    graph.factors_map[assign]->begin() +
                                                                    info.num_factors);
-                std::cout << "try send factors to " << machine_index << std::endl;
-                for (auto fac : vec_facs) std::cout << fac << std::endl;
+                //std::cout << "try send factors to " << machine_index << std::endl;
+                //for (auto& fac : vec_facs) std::cout << fac << std::endl;
                 world.send(machine_index, static_cast<int>(MessageType::Fac), vec_facs);
-                std::cout << "send factors to " << machine_index << " finished" << std::endl;
+                //std::cout << "send factors to " << machine_index << " finished" << std::endl;
             }
         }
     }
 
     std::cout << "rank " << world.rank() << " ready to receive" << std::endl;
     AssignInfo info;
+    std::vector<Factor> facs;
+    std::vector<Variable> vars;
     world.recv(0, static_cast<int>(MessageType::Info), info);
     if (info.num_factors > 0) {
-        std::vector<Factor> facs;
         world.recv(0, static_cast<int>(MessageType::Fac), facs);
         std::cout << "worker" << world.rank() << "receive factors" << std::endl;
         for (auto fac : facs) std::cout << fac << std::endl;
     }
 
     if (info.num_variables > 0) {
-        std::vector<Variable> vars;
         world.recv(0, static_cast<int>(MessageType::Var), vars);
         std::cout << "worker " << world.rank() << " receive variables" << std::endl;
         for (auto var : vars) std::cout << var << std::endl;
+    }
+
+
+    while (--num_samples) {
+        if (world.rank() == 0) {
+            // master node send variables to worker nodes
+            
+            // wait for worker nodes' response
+
+
+        } else {
+            // receive variables from master node
+
+            // worker nodes send combined result to master node
+
+            // wait for
+        }
     }
 
     return 0;
